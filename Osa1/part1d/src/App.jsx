63439baa8a568bbb98b18e5b0c87e65ca8c9940f
fijiro <77334716+fijiro@users.noreply.github.com>
2024-07-21 13:48:18 +0300

    1.6

---
+++ Osa1/part1d/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const App = () => {
+    // tallenna napit omaan tilaansa
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <div>
+                <button onClick={() => { setGood(good + 1) }}> good </button>
+                <button onClick={() => { setNeutral(neutral + 1) }}> neutral </button>
+                <button onClick={() => { setBad(bad + 1) }}> bad </button>
+            </div>
+            <h1>statistics</h1>
+            <div>
+                <div>good {good}</div>
+                <div>neutral {neutral}</div>
+                <div>bad {bad}</div>
+                <div>all</div>
+                <div>average</div>
+                <div>positive X%</div>
+            </div>
+        </div>
+    )
+}
+export default App
