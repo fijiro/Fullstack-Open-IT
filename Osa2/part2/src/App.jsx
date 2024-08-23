779ae982304081bb17fc60c7e57ebf133ba3e1ea
fijiro <77334716+fijiro@users.noreply.github.com>
2024-08-23 23:10:19 +0300

    2.6, 2.7

--- Osa2/part2/src/App.jsx
+++ Osa2/part2/src/App.jsx
@@ -1,3 +1,100 @@
+import React, { useState } from 'react';
+
+function App() {
+    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+    const [newName, setNewName] = useState('')
+
+    const changeName = (event) => {
+        setNewName(event.target.value)
+    }
+    const addPerson = (event) => {
+        event.preventDefault(); // prevent default form send action
+        if (persons.indexOf(newName) < 0) {
+            alert(`${newName} is already added to the phonebook`)
+            return; 
+        }
+        const newPerson = { name: newName }
+        setPersons(persons.concat(newPerson))
+        setNewName("")
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2> 
+            <form onSubmit={addPerson}>
+                <div> 
+                    name: <input value={newName} onChange={changeName} />
+                </div>
+                <div> 
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map((person, index) =>
+                    <li key={index}>{person.name}</li>
+                )}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
+
+
+
+/*
+import { useState } from 'react'
+import Note from './Note'
+
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState('')
+    const [showAll, setShowAll] = useState(false)
+
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            important: Math.random() > 0.5,
+            id: notes.length + 1,
+        }
+
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value)
+    }
+
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important)
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all'}
+                </button>
+            </div>
+            <ul>
+                {notesToShow.map(note =>
+                    <Note key={note.id} note={note} />
+                )}
+            </ul>
+            <form onSubmit={addNote}>
+                <input
+                    value={newNote}
+                    onChange={handleNoteChange}
+                />
+                <button type="submit">save</button>
+            </form>
+        </div>
+    )
+}
 import Course from "./Course.jsx"
 const App = () => {
     const courses = [
@@ -54,4 +151,4 @@ const App = () => {
     )
 }
 
-export default App
+export default App*/
