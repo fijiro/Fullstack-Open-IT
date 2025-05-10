945ae8d82246f444a90609b7042f93f45b140fe9
fijiro <jiro.protonmail@protonmail.com>
2025-05-10 17:46:39 +0300

    3.11

--- Osa3/puhelinluettelo/index.js
+++ Osa3/puhelinluettelo/index.js
@@ -6,10 +6,12 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token("body", (req) => { return req.method === "POST" ? JSON.stringify(req.body) : "" })
+app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+
 
 let persons = [
     {
